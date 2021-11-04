12d87f23292eda01c1af4328efda655947bdae21
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-11-05 00:14:12 +0530

    Completed Login Api Configration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+// User Modal
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register User
@@ -10,13 +17,65 @@ router.post('/', [
   check('email', 'Enter a valid email').isEmail(),
   check('password', 'Enter password with min 7 or more characters').isLength({ min: 7 })
 ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send('User route')
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      // See if user exists:
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exits' }] })
+      }
+      // Get user gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      })
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save the user to database
+      await user.save();
+
+      // Return Webtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          return res.send(res.json({ token }));
+        }
+      );
+
+    }
+    // catch error
+    catch (err) {
+      console.log(err.message);
+      return res.status(500).send('Internal server Error');
+    }
   });
 
 module.exports = router;
