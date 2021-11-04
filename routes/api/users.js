b2fe39ba97d98edb227a6ae663ddf536b3582af5
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-11-04 17:09:35 +0530

    Added User Registration API

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+  check('name', 'Name is required!').not().isEmpty(),
+  check('email', 'Enter a valid email').isEmail(),
+  check('password', 'Enter password with min 7 or more characters').isLength({ min: 7 })
+],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User route')
+  });
 
 module.exports = router;
