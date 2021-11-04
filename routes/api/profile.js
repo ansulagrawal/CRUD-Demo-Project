ed63fabfb2f18bba41ee4b82a402890b6f20f2ae
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-11-05 02:21:26 +0530

    Added (create and update)  Profile Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -25,4 +25,82 @@ router.get('/me', auth, async (req, res) => {
   }
 });
 
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+
+router.post('/', [auth, [
+  check('status', 'Status is required').notEmpty(),
+  check('skills', 'Skills is required').notEmpty(),
+]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      skills,
+      bio,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+      // spread the rest of the fields we don't need to check
+      ...rest
+    } = req.body;
+
+    // build a profile
+    const profileFields = {}
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) profileFields.skills = skills.split(',').map(skills => skills.trim());
+
+
+    // Build social object with profile Fields
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    try {
+      // Check profile exits:
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        // Update profile
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // Create profile
+      profile = await Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
